12fcfc5fbdcbbac715e8a92dd1fa79d5efb13ad0
Bowen Feng <bowenfeng@Bowens-MacBook-Air.local>
2018-05-15 22:44:22 +0800

    Export function upgraded

--- breakdown_value.py
+++ breakdown_value.py
@@ -62,6 +62,16 @@ def Selection():
 		finals.append(q.get())
 	for p in ProcessList:
 		p.join()
+	if StartCellRow_N + 1 == EndCellRow_N:
+		if StartCellColumn == EndCellColumn:
+			ExportExcel(finals)
+		else:
+			ExportExcel(finals)
+	else:
+		if StartCellColumn == EndCellColumn:
+			ExportExcel(finals)
+		else:
+			ExportExcels(finals)
 
 def Files_Row_Column(q, f, ImportFileDirectory, SheetName, i, j):
 	result = [f]
@@ -110,6 +120,14 @@ def CheckWrongItem(Item):
 def ExportExcel(Breakdown):
 	workbook = xlsxwriter.Workbook(ExportFilePath)
 	worksheet = workbook.add_worksheet()
+	for m in range(0, len(Breakdown)):
+		for n in range(0, len(Breakdown[m])):
+			worksheet.write(m, n, Breakdown[m][n])
+	workbook.close()
+
+def ExportExcels(Breakdown):
+	workbook = xlsxwriter.Workbook(ExportFilePath)
+	worksheet = workbook.add_worksheet()
 	for l in range(0,len(Breakdown)):
 		for m in range(0, len(Breakdown[l])):
 			for n in range(0, len(Breakdown[l][m])):
@@ -118,6 +136,3 @@ def ExportExcel(Breakdown):
 
 # body
 Selection()
-
-# export
-ExportExcel(finals)
